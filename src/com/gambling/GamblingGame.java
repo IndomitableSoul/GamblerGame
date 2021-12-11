01a89ab5517fa0edb52e2acb24b19d53ecf134c3
nikita <amarnikita09@gmail.com>
2021-12-11 16:51:21 +0530

    Refactored GamblingGame.java to add UC3

--- src/com/gambling/GamblingGame.java
+++ src/com/gambling/GamblingGame.java
@@ -4,19 +4,29 @@ public class GamblingGame {
        static final int LOSE = 0;
        static final int WIN = 1;
 	public static void main(String[] args) {
-		Gambler gambler1 = new Gambler();
-		int trial = (int)Math.floor(Math.random()*10)%2;
 		
+		Gambler gambler1 = new Gambler();
+		int trialCount=0;
+		double loseStake = Gambler.DAILY_STAKE - (0.5*Gambler.DAILY_STAKE);
+		double winStake = Gambler.DAILY_STAKE - (0.5*Gambler.DAILY_STAKE);
+		
+		while(gambler1.stake!=loseStake && gambler1.stake!=winStake) {
+			System.out.println("Trial "+ ++trialCount);
+	    int trial = (int)Math.floor(Math.random()*10)%2;
+	   
+	    
 		switch (trial) {
-		case LOSE:
+		case LOSE:{
 			gambler1.stake = gambler1.stake - 1;
 			break;
-		case WIN:
+		}
+		case WIN:{
 			gambler1.stake = gambler1.stake + 1;
 			break;
 		}
+		}
 		System.out.println(gambler1.stake);
-
+		}
 	}
 
 }
