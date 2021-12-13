2fb354b3d128d156bb0afcd2ce21fcf63047661b
nikita <amarnikita09@gmail.com>
2021-12-13 13:18:50 +0530

    Refactoring Gambling.java to add a function for gambling

--- src/com/gambling/Gambler.java
+++ src/com/gambling/Gambler.java
@@ -3,6 +3,8 @@ package com.gambling;
 public class Gambler {
 	   static final int DAILY_STAKE = 100;
 	   static final int DAILY_BET = 1;
+	   static final double LOSE_STAKE = DAILY_STAKE - (0.5*DAILY_STAKE);
+	   static final double WIN_STAKE = DAILY_STAKE + (0.5*DAILY_STAKE);
 	   int stake;
 	   public Gambler() {
 		this.stake = DAILY_STAKE;
--- src/com/gambling/GamblingGame.java
+++ src/com/gambling/GamblingGame.java
@@ -3,31 +3,38 @@ package com.gambling;
 public class GamblingGame {
        static final int LOSE = 0;
        static final int WIN = 1;
+        
+       public static void gambling(Gambler gambler1) {
+    	   
+   			
+   	        int trial = (int)Math.floor(Math.random()*10)%2;
+   	   
+   	    
+   		switch (trial) {
+   		case LOSE:{
+   			gambler1.stake = gambler1.stake - 1;
+   			break;
+   		}
+   		case WIN:{
+   			gambler1.stake = gambler1.stake + 1;
+   			break;
+   		}
+   		}
+   		
+   		
+   		
+       }
 	public static void main(String[] args) {
 		
 		Gambler gambler1 = new Gambler();
-		int trialCount=0;
-		double loseStake = Gambler.DAILY_STAKE - (0.5*Gambler.DAILY_STAKE);
-		double winStake = Gambler.DAILY_STAKE + (0.5*Gambler.DAILY_STAKE);
 		
-		while(gambler1.stake!=loseStake && gambler1.stake!=winStake) {
-			System.out.println("Trial "+ ++trialCount);
-	    int trial = (int)Math.floor(Math.random()*10)%2;
-	   
-	    
-		switch (trial) {
-		case LOSE:{
-			gambler1.stake = gambler1.stake - 1;
-			break;
+		while(gambler1.stake!=Gambler.LOSE_STAKE && gambler1.stake!=Gambler.WIN_STAKE) {
+			gambling(gambler1);
+			System.out.println("________________");
 		}
-		case WIN:{
-			gambler1.stake = gambler1.stake + 1;
-			break;
-		}
-		}
-		}
-		System.out.println("Final Stake: "+gambler1.stake);
-		System.out.println("RESIGNING");
+		
+		System.out.println("Final Stake: "+gambler1.stake+ " so Resigning");
+		
 	}
 
 }
